a13df9604e935b871b23b3beb7f5d825c68f17fd
athira12345 <athiraprasad@mca.ajce.in>
2020-07-11 08:08:07 -0800

    fix(server): added log

--- packages/server/server.js
+++ packages/server/server.js
@@ -1,23 +1,24 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const express = require("express");
-const path = require("path");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const config = require("config");
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const config = require('config');
 
 const { HOST, mongoURI } = config;
 // console.log(mongoURI);
 
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+console.log('server running....');
 
 mongoose.connect(mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
 const db = mongoose.connection;
-db.on("error", (error) => console.error(error));
-db.once("open", () => console.log("Connected to Database"));
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to Database'));
 
 const app = express();
 
@@ -27,9 +28,9 @@ app.use(cors());
 // app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use("/uploads", express.static("uploads"));
+app.use('/uploads', express.static('uploads'));
 
-app.use(express.static(path.join(__dirname, "public")));
-const apiRouter = require("./src/routes/api");
-app.use("/api/v1", apiRouter);
-app.listen(HOST, () => console.log("server started"));
+app.use(express.static(path.join(__dirname, 'public')));
+const apiRouter = require('./src/routes/api');
+app.use('/api/v1', apiRouter);
+app.listen(HOST, () => console.log('server started'));
