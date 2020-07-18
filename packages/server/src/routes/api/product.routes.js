5dcce2eb777d204a88964d9c37fabe9f8800034f
Ajish V Nair <ajishvnair55@gmail.com>
2020-07-18 22:04:47 +0530

    fix(server): product image uplaoding

--- packages/server/src/routes/api/product.routes.js
+++ packages/server/src/routes/api/product.routes.js
@@ -3,33 +3,25 @@ const router = express.Router();
 {
     /** for image uploading */
 }
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const multer = require("multer");
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "uploads/");
-    },
-    filename: function (req, file, cb) {
-        cb(
-            null,
-            new Date().toISOString().replace(/:/g, "-") + file.originalname
-        );
+
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+
     },
 });
 
-const fileFilter = (req, file, cb) => {
-    if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
-        cb(null, true);
-    } else {
-        cb(null, false);
-    }
-};
-const upload = multer({
-    storage: storage,
-    limits: {
-        fileSize: 1024 * 1024,
-    },
-    fileFilter: fileFilter,
-});
+const upload = multer({ storage: storage });
 
 const adminAuth = require("../../middlewares/adminAuth");
 const productController = require("../../controller/product.controller");
