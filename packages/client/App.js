dd600632118501de58eb155635cee4a55be8f8bc
Ajish V Nair <ajishvnair55@gmail.com>
2020-07-19 23:23:04 +0530

    fix(client): login fix

--- packages/client/App.js
+++ packages/client/App.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import AppContainer from "./src/navigations/AppNavigation";
-import Login from "./src/screens/Login/Login";
+import Login from "./src/screens/Login/LoginScreen";
 
-import firebase from "firebase";
-import { firebaseConfig } from "./config";
-firebase.initializeApp(firebaseConfig);
+// import firebase from "firebase";
+// import { firebaseConfig } from "./config";
+// firebase.initializeApp(firebaseConfig);
 
 export default function App() {
     const [authenticated, setAuthenticated] = useState(false);
