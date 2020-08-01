796da0a947064d21e61a32619d188c68a380fb0f
Ajish V Nair <ajishvnair55@gmail.com>
2020-08-01 21:59:12 +0530

    added cart products count

--- packages/client/src/components/CartImage/index.js
+++ packages/client/src/components/CartImage/index.js
@@ -1,24 +1,27 @@
 import React from "react";
 import { TouchableOpacity, Image, Text } from "react-native";
+import { useSelector } from "react-redux";
 import PropTypes from "prop-types";
 import styles from "./styles";
 
-export default class MenuImage extends React.Component {
-    render() {
-        return (
-            <TouchableOpacity
-                style={styles.headerButtonContainer}
-                onPress={this.props.onPress}
-            >
-                <Image
-                    style={styles.headerButtonImage}
-                    source={require("../../../assets/icons/cart.png")}
-                />
-            </TouchableOpacity>
-        );
-    }
-}
+const MenuImage = (onPress) => {
+    const cart = useSelector((state) => state.cart);
+    return (
+        <TouchableOpacity
+            style={styles.headerButtonContainer}
+            onPress={onPress}
+        >
+            <Text>{cart.length}</Text>
+            <Image
+                style={styles.headerButtonImage}
+                source={require("../../../assets/icons/cart.png")}
+            />
+        </TouchableOpacity>
+    );
+};
 
 MenuImage.propTypes = {
     onPress: PropTypes.func,
 };
+
+export default MenuImage;
