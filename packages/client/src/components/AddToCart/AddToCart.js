ee9f650bab0d036ad75d1f224784dcf20933f631
Ajish V Nair <ajishvnair55@gmail.com>
2020-08-01 22:22:06 +0530

    set add to cart

--- packages/client/src/components/AddToCart/AddToCart.js
+++ packages/client/src/components/AddToCart/AddToCart.js
@@ -3,12 +3,12 @@ import { TouchableHighlight, Image, Text, View } from "react-native";
 import PropTypes from "prop-types";
 import styles from "./styles";
 
-export default function ({ available }) {
+export default function ({ available, addTo }) {
     return (
         <View style={styles.mainContainer}>
             <TouchableHighlight
                 underlayColor="rgba(73,182,77,1,0.9)"
-                // onPress={this.props.onPress}
+                onPress={() => addTo()}
             >
                 <View style={styles.container}>
                     <Image
--- packages/client/src/screens/Product/BottomNavigator.js
+++ packages/client/src/screens/Product/BottomNavigator.js
@@ -1,11 +1,33 @@
 import React from "react";
 import { View } from "react-native";
+import { useSelector, useDispatch } from "react-redux";
+import { setCart } from "../../state/actions/cart";
 import { checkAvailability } from "../../common/commonMethods";
 import styles from "./styles";
 import QuantitySelector from "../../components/QuantitySelector/QuantitySelector";
 import AddToCart from "../../components/AddToCart/AddToCart";
 
 export default function ({ quantity, total, item }) {
+    const dispatch = useDispatch();
+    const cart = useSelector((state) => state.cart || []);
+    const addTo = () => {
+        if (checkAvailability(item.startTime, item.endTime)) {
+            let newCartList = [...cart];
+            let cartItem = cart.find(
+                (elements) => elements.productId === item._id
+            );
+            if (cartItem) {
+                const index = cart.findIndex(
+                    (elements) => elements.productId === item._id
+                );
+                cartItem.quantity += quantity;
+                newCartList[index] = { ...cartItem };
+            } else {
+                newCartList.push({ productId: item._id, quantity });
+            }
+            dispatch(setCart([...newCartList]));
+        }
+    };
     return (
         <View style={styles.addToCart}>
             <View style={styles.addButtons}>
@@ -19,6 +41,7 @@ export default function ({ quantity, total, item }) {
                 <AddToCart
                     quantity={quantity}
                     available={checkAvailability(item.startTime, item.endTime)}
+                    addTo={addTo}
                 />
             </View>
         </View>
